aef1027f24ad8d871fe11b88a9e4b4f51af620cf
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-07-25 17:08:31 +0000

    Initial commit

---
+++ src/main/java/world/anhgelus/manhunt/Manhunt.java
@@ -0,0 +1,19 @@
+package world.anhgelus.manhunt;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Manhunt implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LogManager.getLogger("manhunt");
+    public static final String MOD_ID = "manhunt";
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
